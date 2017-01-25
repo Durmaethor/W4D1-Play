efe03640d0446415bdd20e4ea6bd550fbad52a35
Ryan Kramer <rkramer613@gmail.com>
2017-01-24 17:25:10 -0700

    Add some odd callback functions

--- code.js
+++ code.js
@@ -2,4 +2,31 @@ function sayHi() {
     alert("Hi");
 }
 
-//setInterval(sayHi, 3000);
+setInterval(sayHi, 3000);
+
+
+
+
+
+var myList = [1,2,3,4,5];
+
+function forEach(list, callback) {
+
+}
+
+function (myList, function(el) {
+    alert(el);
+})
+
+
+
+//Example 
+
+function alertSomething(callback) {
+    alert(callback());
+}
+
+alertSomething(function()) {
+    return 'Hello';
+}
+
